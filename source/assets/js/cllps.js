c1ab6195c611bbc210dca8afaa83b2371ee79f28
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2023-04-08 18:16:01 +0700

    Delete source directory
    Signed-off-by: NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>

--- source/assets/js/cllps.js
+++
@@ -1,5 +0,0 @@
-var trg = document.querySelector(".cllps .trg");
-var cllps = document.querySelector(".cllps");
-trg.onclick = function(){
-cllps.classList.toggle("active");
-}
--- source/assets/js/nav.js
+++
@@ -1,5 +0,0 @@
-var trg = document.querySelector(".trg");
-var nav = document.querySelector("nav");
-trg.onclick = function(){
-nav.classList.toggle("active");
-}
