f43d3fbc74cd88492123f2b7b1850aeede752ed0
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2023-04-16 21:29:05 +0700

    Delete nav-cllps.js
    Signed-off-by: NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>

--- assets/js/nav-cllps.js
+++
@@ -1,5 +0,0 @@
-var trg = document.getElementById("nav-cllps--projects");
-var cllps = document.querySelector(".menu[type=projects]");
-trg.onclick = function(){
-cllps.classList.toggle("active");
-}
